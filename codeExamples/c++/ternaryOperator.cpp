237ad813ca15da1f1ba6fb39aa0387e1d1603137
Zhidkovgv <zhidkov.gleb@gmail.com>
2018-09-29 21:43:50 +0300

    update: add example for ? operator

--- codeExamples/c++/ternaryOperator.cpp
+++ codeExamples/c++/ternaryOperator.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 
 int main () {
-    
-    return 0;
+    bool are_you_gay;
+    cin >> are_you_gay;
+    cout << (are_you_gay ? "Welcome to blue boat" : "Get the fuck out of here") << endl;
+    return 0;   
 }
 
