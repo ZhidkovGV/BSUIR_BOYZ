4595672e6c32fe6b83c501b2dbb2383f307d5fbb
Zhidkovgv <zhidkov.gleb@gmail.com>
2018-09-29 22:03:03 +0300

    update: add example for recursive functions

--- codeExamples/c++/functions.cpp
+++ codeExamples/c++/functions.cpp
@@ -16,7 +16,6 @@ int double_number (int number) {
 int main () {
     int some_number = 5;
     int& ref_to_some_number = some_number; 
-    int some_numbers[] = [1, 2, 3, 4, 5];
     output_message("we have some int varieble with value ", some_number);
     int doubled_number = double_number(some_number);
     output_message("now we created new varieble, and its value: ", doubled_number);
--- codeExamples/c++/recusrsive.cpp
+++ codeExamples/c++/recusrsive.cpp
@@ -2,8 +2,23 @@
 
 using namespace std;
 
+int count_factorial(int number) {
+    for (int i = (number - 1); i > 1; i--) {
+        number = number * i;
+    }
+    return number;
+}
+int count_factorial_recursive(int number) {
+    if (number > 1) {
+        number = number * count_factorial_recursive(number -1);
+    }
+    return number;
+}
 int main () {
-    
+    int number = 1;
+    cin >> number;
+    cout << "recursive function result: " << count_factorial_recursive(number) << endl;
+    cout << "interable function result: " << count_factorial(number) << endl;
     return 0;
 }
 
