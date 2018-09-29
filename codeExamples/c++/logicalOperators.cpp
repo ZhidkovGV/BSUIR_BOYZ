a6e05b7baf7b855f89956215d6c0ea1fbc0f3a95
Zhidkovgv <zhidkov.gleb@gmail.com>
2018-09-29 21:52:37 +0300

    update: add example for logial operators

--- codeExamples/c++/logicalOperators.cpp
+++ codeExamples/c++/logicalOperators.cpp
@@ -3,7 +3,14 @@
 using namespace std;
 
 int main () {
-    
+    bool some_true_value = true;
+    bool some_false_value = false;
+
+    cout << "some_true_value || some_false_value is " << (some_true_value || some_false_value ? "true" : "false") << endl; 
+    cout << "some_false_value || some_true_value is " << (some_false_value || some_true_value ? "true" : "false") << endl;
+    cout << "some_false_value && some_true_value is " << (some_false_value && some_true_value ? "true" : "false") << endl;
+    cout << "!some_false_value is " << (!some_false_value ? "true" : "false") << endl;
+    cout << "!some_true_value is " << (!some_true_value ? "true" : "false") << endl;
     return 0;
 }
 
