88200cc121da66856af36d661018f1392449c83d
Zhidkovgv <zhidkov.gleb@gmail.com>
2018-09-29 22:00:32 +0300

    update: add example for matrix

--- codeExamples/c++/multiDimesionalArrays.cpp
+++ codeExamples/c++/multiDimesionalArrays.cpp
@@ -3,7 +3,28 @@
 using namespace std;
 
 int main () {
-    
+    int length = 10;
+    int** matrix = new int* [length];
+
+    for (int i = 0; i < length; i++) {
+        matrix[i] = new int[length];
+    }
+
+    for (int i = 0; i < length; i++) {
+        for (int j = 0; j < length; j++) {
+            matrix[i][j] = sqrt(pow((i - j), 2));
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < length; i++) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+
     return 0;
 }
 
