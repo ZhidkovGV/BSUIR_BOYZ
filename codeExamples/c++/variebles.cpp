de920b6219b1fe3a38a2a27e5899ecd6ac3d54fd
Zhidkovgv <zhidkov.gleb@gmail.com>
2018-09-29 21:36:16 +0300

    update: variebles example

--- codeExamples/c++/variebles.cpp
+++ codeExamples/c++/variebles.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-
 using namespace std;
 
-int main () {
-    
+int main() {
+    int simple_number = 5;
+    char simpleCharacter = 'A';
+    double simpleFloat = 1.337;
+    bool simpleBool = true;
     return 0;
 }
+//kebab_case
+//camelCase
 
