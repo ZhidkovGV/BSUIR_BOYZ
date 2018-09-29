d17814bea368e8fc9a24ad92d6893f78394f8ea0
Zhidkovgv <zhidkov.gleb@gmail.com>
2018-09-29 21:37:27 +0300

    update: refsAndPointers example

--- codeExamples/c++/refsAndPointers.cpp
+++ codeExamples/c++/refsAndPointers.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 
 int main () {
-    
-    return 0;
+   int some_number = 0;
+   int* some_number_adress = &some_number; 
+   cout << some_number_adress << endl;
+   return 0;
 }
 
