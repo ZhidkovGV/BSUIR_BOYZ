d14ce43bcdc94a611897f3c97cc394c01a96472d
Zhidkovgv <zhidkov.gleb@gmail.com>
2018-09-29 21:41:25 +0300

    fix: file name and update example

--- codeExamples/c++/ifConditon.cpp
+++ codeExamples/c++/ifConditon.cpp
@@ -3,7 +3,13 @@
 using namespace std;
 
 int main () {
-    
+    int some_number = 0;
+    cin >> some_number;
+    if (some_number != 0) {
+        cout << "You changed number!" << endl;
+    } else {
+        cout << "Number is still zero!" << endl;
+    }
     return 0;
 }
 
