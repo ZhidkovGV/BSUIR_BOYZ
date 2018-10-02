bdf1ada3502d5f6fcc925b673b446d92d26a5564
ZhidkovGV <zhidkov.gleb@gmail.com>
2018-10-02 22:17:53 +0300

    update: structure of game of life

--- codeExamples/c++/GameOfLife/GameOfLife.cpp
+++ codeExamples/c++/GameOfLife/GameOfLife.cpp
@@ -9,17 +9,26 @@ int** init() {
 void apply_rules (int** gameField) {
 
 }
-bool end_of_game (int** gameField ) {
+
+bool end_of_game (int** currentGameField, int** prevGameField) {
 
 }
+
 void draw_matrix (int** matrixToDraw) {
 
 }
+void update_prev_matrix (int** currentGameField, int** prevGameField) {
+
+}
+
 void game_loop () {
     int ** gameOfLifeMatrix = init();
-    while (!end_of_game(gameOfLifeMatrix)) {
+    int ** prevMatrix;
+    update_prev_matrix(gameOfLifeMatrix, prevMatrix);
+    while (!end_of_game(gameOfLifeMatrix, prevMatrix)) {
         apply_rules(gameOfLifeMatrix);
         draw_matrix(gameOfLifeMatrix);
+        update_prev_matrix(gameOfLifeMatrix, prevMatrix);
     }
 }
 
