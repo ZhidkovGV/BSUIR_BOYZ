8c283098531ea876b23b6e6b84bb527a42338e34
ZhidkovGV <zhidkov.gleb@gmail.com>
2018-10-03 21:36:06 +0300

    feat: draw and init functions implemented

--- codeExamples/c++/GameOfLife/GameOfLife.cpp
+++ codeExamples/c++/GameOfLife/GameOfLife.cpp
@@ -2,12 +2,24 @@
 
 using namespace std;
 
+
+int GLOBAL_LENGTH = 0;
+
 int** init() {
-    int length = 0;
     int ** matrix;
-    cin >> length;
-    matrix = new int* [length];
-    
+    cin >> GLOBAL_LENGTH;
+
+    matrix = new int* [GLOBAL_LENGTH];
+    for (int i = 0; i < GLOBAL_LENGTH; i++) {
+        matrix[i] = new int [GLOBAL_LENGTH];
+    }
+
+    for (int i = 0; i < GLOBAL_LENGTH; i++) {
+        for (int j = 0; j < GLOBAL_LENGTH; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
     return matrix;
 }
 
@@ -16,32 +28,40 @@ void apply_rules (int** gameField) {
 }
 
 bool end_of_game (int** currentGameField, int** prevGameField) {
-
+    return false;
 }
 
 void draw_matrix (int** matrixToDraw) {
-    cout << sizeof(matrixToDraw) / sizeof(matrixToDraw[0]) << endl;
+    for (int i = 0; i < GLOBAL_LENGTH; i++) {
+        for (int j = 0; j < GLOBAL_LENGTH; j++) {
+            cout << matrixToDraw[i][j];
+        }
+        cout << endl;
+    }
 }
+
 void update_prev_matrix (int** currentGameField, int** prevGameField) {
 
 }
+
 void clear_matrix (int **matrix) {
 
 }
+
 void game_loop () {
     int ** gameOfLifeMatrix = init();
     int ** prevMatrix;
     update_prev_matrix(gameOfLifeMatrix, prevMatrix);
     while (!end_of_game(gameOfLifeMatrix, prevMatrix)) {
+        update_prev_matrix(gameOfLifeMatrix, prevMatrix);
         apply_rules(gameOfLifeMatrix);
         draw_matrix(gameOfLifeMatrix);
-        update_prev_matrix(gameOfLifeMatrix, prevMatrix);
     }
     clear_matrix(gameOfLifeMatrix);
     clear_matrix(prevMatrix);
 }
 
 int main () {
-    
+    game_loop();
     return 0;
 }
