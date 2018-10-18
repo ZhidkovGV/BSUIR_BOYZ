14ee92b61aa43877390773c48cde2d86271c8272
ZhidkovGV <zhidkov.gleb@gmail.com>
2018-10-18 23:32:17 +0300

    some js examples WIP

---
+++ codeExamples/JS/projExample/source.js
@@ -0,0 +1,14 @@
+class Human {
+    sasat (dick) {
+        return dick
+    }
+    constructor(size, dick) {
+        this.shoesSize = size;
+        this.haveDick = dick;
+    }
+    
+}
+
+class ItiuvtsHuman extends Human {
+    
+}
