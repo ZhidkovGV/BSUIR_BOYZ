fd23721e17d3857c01c9cb50ad5de30c453fec29
ZhidkovGV <zhidkov.gleb@gmail.com>
2018-10-24 22:56:25 +0300

    update: tree methods prototypes

--- codeExamples/JS/bst/bst.js
+++ codeExamples/JS/bst/bst.js
@@ -2,12 +2,24 @@ class BinarySearchTree {
     constructor (value) {
         this.root = new TreeNode()
     }
+
+    addNode(value) {
+
+    }
+
+    deleteNode(value) {
+
+    }
+
+    searchPosition(node) {
+
+    }
 }
 
 class TreeNode {
     constructor (value, leftChild = null, rightChild = null) {
         this.value = value;
         this.leftChild = leftChild;
-        this.rightChild =rightChild;
+        this.rightChild = rightChild;
     }
 }
