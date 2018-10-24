32e044cea6deaf77d78dc955ee08e794e53d51d0
ZhidkovGV <zhidkov.gleb@gmail.com>
2018-10-24 23:53:24 +0300

    update: bst dd node

--- codeExamples/JS/bst/bst.js
+++ codeExamples/JS/bst/bst.js
@@ -1,25 +1,31 @@
-class BinarySearchTree {
-    constructor (value) {
-        this.root = new TreeNode()
+    class BinarySearchTree {
+        constructor (value) {
+            this.root = new TreeNode(value)
+        }
+
+        addNode(value, node = this.root) {
+            if (!node.leftChild && node.value > value) {
+                node.leftChild = new TreeNode(value);
+                return this;
+            }
+            if (!node.rightChild && node.value < value) {
+                node.rightChild = new TreeNode(value);
+                return this; 
+            }
+            return this.addNode(value, value > node.value ? node.rightChild : node.leftChild); 
+        }
+
+        deleteNode(value) {
+            return this
+        }
+
+        searchNode (node, value) {
     }
 
-    addNode(value) {
-
+    class TreeNode {
+        constructor (value, leftChild = null, rightChild = null) {
+            this.value = value;
+            this.leftChild = leftChild;
+            this.rightChild = rightChild;
+        }
     }
-
-    deleteNode(value) {
-
-    }
-
-    searchPosition(node) {
-
-    }
-}
-
-class TreeNode {
-    constructor (value, leftChild = null, rightChild = null) {
-        this.value = value;
-        this.leftChild = leftChild;
-        this.rightChild = rightChild;
-    }
-}
